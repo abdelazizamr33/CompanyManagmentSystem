4bdc9875fb39ca887ee456bb936c7be2d9ea92bc
abdelazizamr33 <zizoamr920@gmail.com>
2025-03-09 14:23:21 +0200

    Department Module BLL

---
+++ Company.Route.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using Company.Route.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Route.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department? Get(int id);
+
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+
+    }
+}
---
+++ Company.Route.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,44 @@
+﻿using Company.Route.BLL.Interfaces;
+using Company.Route.DAL.Data.Contexts;
+using Company.Route.DAL.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Route.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _context; // readonly -> make no one can assign value to it
+        public DepartmentRepository()
+        {
+            _context = new CompanyDbContext();
+        }
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+        public Department? Get(int id)
+        {
+            return _context.Departments.Find(id);
+        }
+        public int Add(Department model)
+        {
+            _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+        public int Update(Department model)
+        {
+            _context.Departments.Update(model);
+            return _context.SaveChanges();
+        }
+        public int Delete(Department model)
+        {
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+    }
+}
