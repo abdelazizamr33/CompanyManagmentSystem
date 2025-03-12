8401dde7a8c28aa9362a862e24e502f56a9a036b
abdelazizamr33 <zizoamr920@gmail.com>
2025-03-12 23:37:46 +0200

    Refactored code in BLL based on Generic

--- Company.Route.DAL/Models/Department.cs
+++ Company.Route.DAL/Models/Department.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Company.Route.DAL.Models
 {
-    public class Department
+    public class Department:BaseEntity
     {
         public int Id { get; set; }
         public string Code { get; set; }
