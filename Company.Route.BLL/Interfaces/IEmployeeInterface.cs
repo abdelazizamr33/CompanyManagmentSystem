cc733ceea9ba29b675a7998169b4818c0334e0ce
abdelazizamr33 <zizoamr920@gmail.com>
2025-03-12 23:18:51 +0200

    implement of BLL for Employee before Generic

---
+++ Company.Route.BLL/Interfaces/IEmployeeInterface.cs
@@ -0,0 +1,19 @@
+﻿using Company.Route.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Route.BLL.Interfaces
+{
+    public interface IEmployeeInterface
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? Get(int id);
+
+        int Add(Employee model);
+        int Update(Employee model);
+        int Delete(Employee model);
+    }
+}
